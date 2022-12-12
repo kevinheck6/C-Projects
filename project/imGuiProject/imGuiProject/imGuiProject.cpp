4b8b5fa837fa661ef0c058272519b67f5116f2dd
kevinheck6 <kevinheck941972@gmail.com>
2022-12-12 16:02:40 -0300

    Initial commit

---
+++ project/imGuiProject/imGuiProject/imGuiProject.cpp
@@ -0,0 +1,12 @@
+﻿// imGuiProject.cpp : Defines the entry point for the application.
+//
+
+#include "imGuiProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
